e72bd308c975f0f55fa21df8f3e1e2360c78a3a7
Just-love-myself <ehrbs7181@gmail.com>
2024-06-09 17:11:49 +0900

    #22 fix : EncryptionService 암호화 방식 보완

--- ConvoPersona/src/main/java/OSS_group11/ConvoPersona/services/EncryptionService.java
+++ ConvoPersona/src/main/java/OSS_group11/ConvoPersona/services/EncryptionService.java
@@ -1,21 +1,31 @@
 package OSS_group11.ConvoPersona.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Service
 public class EncryptionService {
 
-    @Value("${encryption.algorithm}")
-    private static String algorithm;
+    private final String algorithm;
+    private final String key;
+    private final String iv;
 
-    @Value("${encryption.key}")
-    private static String key;
+    @Autowired
+    public EncryptionService(@Value("${encryption.algorithm}") String algorithm,
+                             @Value("${encryption.key}") String key,
+                             @Value("${encryption.iv}") String iv) {
+        this.algorithm = algorithm;
+        this.key = key;
+        this.iv = iv;
+    }
 
 
     /***
@@ -23,7 +33,16 @@ public class EncryptionService {
      * @return
      */
     private SecretKey getSecretKey() {
-        return new SecretKeySpec(key.getBytes(), algorithm);
+        return new SecretKeySpec(key.getBytes(), "AES");
+    }
+
+    /**
+     * IV 생성 (IV Generation)
+     *
+     * @return IvParameterSpec
+     */
+    private IvParameterSpec getIvSpec() {
+        return new IvParameterSpec(iv.getBytes(StandardCharsets.UTF_8));
     }
 
     /***
@@ -34,8 +53,8 @@ public class EncryptionService {
      */
     public String encrypt(String plainText) throws Exception {
         Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey());
-        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(), getIvSpec());
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
@@ -47,9 +66,9 @@ public class EncryptionService {
      */
     public String decrypt(String encryptedText) throws Exception {
         Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.DECRYPT_MODE, getSecretKey());
+        cipher.init(Cipher.DECRYPT_MODE, getSecretKey(), getIvSpec());
         byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
         byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-        return new String(decryptedBytes);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
     }
 }
